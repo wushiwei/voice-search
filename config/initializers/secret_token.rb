11fb83c6e55a755a02ec367af93c1a28fc8044d0
Dong Qingshan <dongqs@gmail.com>
2012-10-14 21:44:44 +0800

    installed rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VoiceSearch::Application.config.secret_token = '0ef6906d5ca04a8060ffa22baa5236ab0ea7a3d874f921c69a9bcc67c8a1f4223bdb4f6b8c281b7be866d65d2564757b4d25199751d5d04fbdc5e90550c80df6'
